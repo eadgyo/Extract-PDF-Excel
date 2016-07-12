9029bf1af00840dcae966ff49a7a6c68989f5276
eadgyo <eadgyo@gmail.com>
2016-07-12 12:09:15 +0200

    Active travis testing

---
+++ src/main/java/org/cora/extractPdfExcel/Main.java
@@ -0,0 +1,14 @@
+package org.cora.searchengine;
+
+import java.io.IOException;
+
+/**
+ * Created by eadgyo on 12/07/16.
+ */
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+
+    }
+}
