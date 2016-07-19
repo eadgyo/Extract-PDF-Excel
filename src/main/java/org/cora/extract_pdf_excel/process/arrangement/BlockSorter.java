7812f3d4502ddf0be2776ad59c1b68ccd70c3cfa
eadgyo <eadgyo@gmail.com>
2016-07-19 23:02:46 +0200

    End writing of main splitting Process Start Unit testing on splitting process Start Tools

--- src/main/java/org/cora/extract_pdf_excel/process/arrangement/BlockSorter.java
+++ src/main/java/org/cora/extract_pdf_excel/process/arrangement/BlockSorter.java
@@ -15,6 +15,13 @@ import java.util.Set;
 /**
  * Created by eadgyo on 17/07/16.
  */
+
+/**
+ * Tools to sort blocks in lane
+ *
+ * Use oppositeAxis for lane insertion in lanes
+ * Use axis for block insertion in lane
+ */
 public class BlockSorter
 {
     /**
@@ -42,15 +49,16 @@ public class BlockSorter
             // While there are blocks colliding along lane axis
             while (collidingBlock != null)
             {
-                // If a good higher lane exists, use this one, else split lower lane
-                Lane insertedLane = useExistingOrCreateLane(axis,
-                                                            oppositeAxis,
-                                                            block,
-                                                            collidingBlock,
-                                                            lowerLane,
-                                                            higherLane,
-                                                            lanes,
-                                                            savedCollidingBlock);
+                // We have to find another lane to handle prevent collision
+                // If a good higher lane exists, use this one, else split lower lane in 2 lanes
+                Lane insertedLane = useExistingOrSplitLane(axis,
+                                                           oppositeAxis,
+                                                           block,
+                                                           collidingBlock,
+                                                           lowerLane,
+                                                           higherLane,
+                                                           lanes,
+                                                           savedCollidingBlock);
                 // If insert lane has changed
                 if (insertedLane != lowerLane)
                 {
@@ -65,6 +73,11 @@ public class BlockSorter
                 collidingBlock = getBlockCollidingInLane(axis, block, lowerLane);
             }
 
+            // Insert block in lowerLane
+            lowerLane.addBlockAndFitLane(axis, oppositeAxis, block);
+            // Make sure lowerLane is not colliding with his higherLane
+            lowerLane.fitToHigherLane(oppositeAxis, higherLane);
+
             // Reinsert all removed blocks
             for (Block removedBlock : savedCollidingBlock)
             {
@@ -73,7 +86,8 @@ public class BlockSorter
         }
         else
         {
-            // Correct lane does no exist
+            // Correct lane does no exist, we have to create a new Lane
+
             // Create a new lane with block rect as lane bound
             Lane lane = new Lane();
             // Force use of block
@@ -171,34 +185,49 @@ public class BlockSorter
             return null;
     }
 
-    private static Lane useExistingOrCreateLane(int axis,
-                                                int oppositeAxis,
-                                                Block block,
-                                                Block collidingBlock,
-                                                Lane lowerLane,
-                                                Lane higherLane,
-                                                Lanes lanes, Set<Block> savedCollidingBlock)
+    /**
+     * Use higher lane or create a new lane to prevent collision between block and collidingBlock
+     * Used to prevent collision of two blocks.
+     *
+     * @param axis                 lane axis
+     * @param oppositeAxis         opposite lane axis
+     * @param insertedBlock        inserted block
+     * @param collidingBlock       existing block
+     * @param lowerLane            lower than higherLane
+     * @param higherLane           higher than lowerLane
+     * @param lanes                multiples lanes
+     * @param savedCollidingBlocks saved removed blocks for future reinsertion
+     *
+     * @return lane to insert the block
+     */
+    private static Lane useExistingOrSplitLane(int axis,
+                                               int oppositeAxis,
+                                               Block insertedBlock,
+                                               Block collidingBlock,
+                                               Lane lowerLane,
+                                               Lane higherLane,
+                                               Lanes lanes, Set<Block> savedCollidingBlocks)
     {
         // Take the inserted block or existing block, with higher coordinate
-        // Get the right most object on opposite axis
+        // Get position of inserted block relative to colliding block
         Direction relativeDirection = getRelativeDirection(oppositeAxis,
-                                                           block.getBound(),
+                                                           insertedBlock.getBound(),
                                                            collidingBlock.getBound());
-        // Get lower and higher block
         Block lowerBlock, higherBlock;
-        Lane usedLane;
 
-        double oldLowerKey = lowerLane.getPos(oppositeAxis);
-
-        // If the colliding block is at the right
-        if (relativeDirection == Direction.RIGHT)
+        // If the inserted block is at the right of the colliding block
+        if (relativeDirection == Direction.LEFT)
         {
-
-            lowerBlock = block;
+            lowerBlock = insertedBlock;
             higherBlock = collidingBlock;
 
+            // Save lowerLane to update his key in lanes
+            double oldLowerKey = lowerLane.getPos(oppositeAxis);
+
+            // Colliding block have to change lane
             // Remove first collidingBlock from his lane
             Block removed = lowerLane.remove(higherBlock.getPos(axis));
+            // Make sure colliding block key pointed to himself in his lane
             assert (removed == higherBlock);
 
             // If higher lane exists and is colliding with higherBlock
@@ -207,13 +236,13 @@ public class BlockSorter
             {
                 // Don't create a new lane
 
-                // If the higherBlock is the collidingBlock
+                // Save higherLane to update his key in lanes
                 double oldHigherKey = higherLane.getPos(oppositeAxis);
 
                 // Save colliding block for future reinsert, preventing collision with higherLane's blocks
-                savedCollidingBlock.add(collidingBlock);
+                savedCollidingBlocks.add(collidingBlock);
 
-                // Update lowerLane size to update removed block
+                // Update lowerLane size to update lane bounds after removing colliding block
                 lowerLane.resetAndFitLaneToInBlocks(axis, oppositeAxis);
                 // Prevent lane collision by fitting end of lowerLane to start of higherLane
                 lowerLane.fitToHigherLane(oppositeAxis, higherLane);
@@ -221,64 +250,91 @@ public class BlockSorter
                 // If higherLane key has changed
                 if (oldHigherKey != higherLane.getPos(oppositeAxis))
                 {
-                    // Remove and replace higherLane after change
+                    // Update his key in lanes
                     lanes.replaceKey(oppositeAxis, oldHigherKey, higherLane);
                 }
             }
             else
             {
-                createAndInsertLane(axis, oppositeAxis, lowerBlock, higherBlock, lowerLane);
+                // Can't add collidingBlock in higherLane because higherLane is after collidingBlock along
+                // oppositeAxis.
+                // Create a new lane to insert collidingBlock
+                splitLowerLaneAndReorderBlocks(axis, oppositeAxis, lowerBlock, higherBlock, lowerLane, true);
             }
 
-            usedLane = lowerLane;
+            // Use lowerLane to insert inserted block
+            return lowerLane;
         }
         else
         {
             lowerBlock = collidingBlock;
-            higherBlock = block;
+            higherBlock = insertedBlock;
+
+            // Do not need to remove higherBlock as it's not in a lane
 
             // If higher lane does not exist or is not colliding with higherBlock
             if (higherLane == null || !isCollidingWithBlock(oppositeAxis, higherBlock, higherLane))
             {
-                higherLane = createAndInsertLane(axis, oppositeAxis, lowerBlock, higherBlock, lowerLane);
+                // Split lower lane and reorder lower lane's blocks
+                higherLane = splitLowerLaneAndReorderBlocks(axis,
+                                                            oppositeAxis,
+                                                            lowerBlock,
+                                                            higherBlock,
+                                                            lowerLane,
+                                                            false);
             }
 
-            usedLane = higherLane;
+            // Use higherLane to insert inserted block
+            return higherLane;
         }
-
-        return usedLane;
     }
 
     /**
-     * Create a new line and insert
-     * @param axis lane axis
-     * @param oppositeAxis opposite lane axis
-     * @param lowerBlock lower than higher block
-     * @param higherBlock higher than lower block
-     * @param lowerLane lower lane splitted in lanes
+     * Split lowerLane and reorder lower lane in blocks
+     *
+     * @param axis                   lane axis
+     * @param oppositeAxis           opposite lane axis
+     * @param lowerBlock             lower than higher block
+     * @param higherBlock            higher than lower block
+     * @param lowerLane              lower lane splitted in lanes
+     * @param forceResizingLowerLane force update the size of lowerLane
+     *
      * @return created higher lane
      */
-    private static Lane createAndInsertLane(int axis,
-                                            int oppositeAxis,
-                                            Block lowerBlock,
-                                            Block higherBlock,
-                                            Lane lowerLane)
+    private static Lane splitLowerLaneAndReorderBlocks(int axis,
+                                                       int oppositeAxis,
+                                                       Block lowerBlock,
+                                                       Block higherBlock,
+                                                       Lane lowerLane,
+                                                       boolean forceResizingLowerLane)
     {
         // HigherLane cannot be used to reinsert higherBlock
         // Create a new lane and insert it in lanes
         Lane higherLane = new Lane();
         higherLane.fitLane(axis, oppositeAxis, higherBlock);
 
-        // Save lower lane
-        Set<Block> savedBlocks = lowerLane.copyBlocks();
+        // If the start of lower lane is before end of lowerLane, there are colliding
+        if (lowerLane.getPos(oppositeAxis) + lowerLane.getLength(oppositeAxis) > higherLane.getPos(oppositeAxis))
+        {
+            // Need to reinsert all blocks
 
-        // Clear blocks in lane
-        lowerLane.clearBlocks();
-        // Reset lane bound to lowerBlock
-        lowerLane.setRectangle(lowerBlock.getBound());
+            // Save lower lane
+            Set<Block> savedBlocks = lowerLane.copyBlocks();
 
-        // Reinsert all saved blocks in lowerLane or higherLane
-        insertBlocksInRightLane(axis, oppositeAxis, savedBlocks, lowerLane, higherLane);
+            // Clear blocks in lane
+            lowerLane.clearBlocks();
+            // Reset lane bound to lowerBlock
+            lowerLane.setRectangle(lowerBlock.getBound());
+
+            // Reinsert all saved blocks in lowerLane or higherLane
+            insertBlocksInRightLane(axis, oppositeAxis, savedBlocks, lowerLane, higherLane);
+        }
+        else if (forceResizingLowerLane)
+        {
+            // HigherLane is not colliding with LowerLane.
+            // But force resize lower lane using in blocks
+            lowerLane.resetAndFitLaneToInBlocks(axis, oppositeAxis);
+        }
 
         return higherLane;
     }
@@ -329,13 +385,13 @@ public class BlockSorter
     }
 
     /**
-     * Get relative direction of rect1 to rect2
+     * Get position of rect1 relative to rect2
      *
      * @param oneAxis used axis
      * @param rect1   first rectangle
      * @param rect2   second rectangle
      *
-     * @return relative direction of rect1 to rect2, if they have the same lower position, rect1 is considered at the
+     * @return positon of rect1 relative to rect2, if they have the same lower position, rect1 is considered at the
      * left of rect2
      */
     private static Direction getRelativeDirection(int oneAxis, Rect rect1, Rect rect2)
---
+++ src/main/java/org/cora/extract_pdf_excel/tools/DefaultBlockRemover.java
@@ -0,0 +1,12 @@
+package org.cora.extract_pdf_excel.tools;
+
+/**
+ * Created by eadgyo on 19/07/16.
+ */
+
+/**
+ * Remove block if it already exists
+ */
+public class DefaultBlockRemover
+{
+}
